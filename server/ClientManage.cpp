5ab9aa4289d455737d26f7e80acaa9cbb61b2aa8
Alyson NGONYAMA <angonyam@e4r16p1.wethinkcode.co.za>
2018-10-26 16:15:39 +0200

    creating client management system

--- server/ClientManage.cpp
+++ server/ClientManage.cpp
@@ -2,10 +2,39 @@
 
 #include "ClientManage.hpp"
 
+/*
+** 	Sets up the socket list each loop as well as sets STDIN for listening
+**  so user can write commands and dispatch them to clients.
+*/
+
 void			ClientManage::ZeroBotList(void)
 {
 	FD_ZERO(&this->botlist);
 	for (auto const& port : std::port_list)
+	{
 		FD_SET(port, &(this->botlist));
+		if (port > this->fd_max)
+			this->fd_max = port;
+	}
+	FD_SET(0, &(this->botlist));
+}
+
+/*
+** 	Main listening loop. Ports are monitored with SELECT for any incoming.
+** 	connections from remote bots. 
+*/
+
+void			ClientManage::CommandLoop(void)
+{
+	int			r;
+
+	for (;;)
+	{
+		r = select(this->fd_max + 1, &this->botlist, NULL, NULL, NULL);
+		if (r)
+		{
+
+		}
+	}
 }
 
